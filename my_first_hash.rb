b314190b83c33be34027be940eff60972ce70a3a
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-31 23:28:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,18 +12,17 @@ end
 
 
 
-def retrieval(shipping_manifest)
+def retrieval 
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
 end
-retrieval[key]
-end
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+retrieval["oil painting"]
+end
 
 def adding
   shipping_manifest = {
