5e22660a7c75b3165c63fde67fb0871c1ccd5a11
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-31 23:18:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,12 +18,12 @@ def retrieval(shipping_manifest)
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+shipping_manifest = ["whale bone corsets", "porcelain vases", "oil paintings"]
+end
+shipping_manifest [2]
+end
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
-end
-shipping_manifest["oil paintings"]
-end
 
 def adding
   shipping_manifest = {
